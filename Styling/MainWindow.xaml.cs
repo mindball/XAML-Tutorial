bdae1d99a8867e0bf8e1ce35e9896c4d54b1ffe7
mindball <mincho.balaliew@gmail.com>
2021-12-02 12:45:36 +0200

    Styling

---
+++ Styling/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Styling.TriggersDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Styling
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.secondRect.Fill = (SolidColorBrush)this.Resources["brushResourceCustomColor"];
+        }
+
+        private void Trigers_Click(object sender, RoutedEventArgs e)
+        {
+            TriggersWPF tr = new TriggersWPF();
+            tr.Show();
+        }
+    }
+}
---
+++ Styling/TriggersDemo/TriggersWPF.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Styling.TriggersDemo
+{
+    /// <summary>
+    /// Interaction logic for TriggersWPF.xaml
+    /// </summary>
+    public partial class TriggersWPF : Window
+    {
+        public TriggersWPF()
+        {
+            InitializeComponent();
+        }
+    }
+}
