7215a98e11d1072cd8cd53b19ea2ff7268a2f6d2
mindball <mincho.balaliew@gmail.com>
2022-01-30 08:44:28 +0200

    avoid duplicate subscribe

---
+++ EventDeclarationAvoidDuplicateSubscribers/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDeclarationAvoidDuplicateSubscribers
+{
+    public class TestClass : INotifyPropertyChanged
+    {
+        private event PropertyChangedEventHandler _PropertyChanged;
+        private List<PropertyChangedEventHandler> subscribers =
+            new List<PropertyChangedEventHandler>();
+
+        public event PropertyChangedEventHandler PpropertyChanged
+        {
+            add
+            {
+                if (!subscribers.Contains(value))
+                {
+                    _PropertyChanged += value;
+                    subscribers.Add(value); 
+
+                }
+            }
+            remove
+            {
+                _PropertyChanged -= value;
+                subscribers.Remove(value);  
+            }
+        }
+
+
+    }
+}
